aebca8a3d560c4df79b1d9aad1ad5fec15890a61
David Sinclair <David.Sinclair@scotland.gsi.gov.uk>
2015-04-15 14:27:37 +0100

    sonar issue

--- decommission-tool-service/src/main/java/org/mygovscot/decommissioned/repository/PageRepository.java
+++ decommission-tool-service/src/main/java/org/mygovscot/decommissioned/repository/PageRepository.java
@@ -4,7 +4,6 @@ package org.mygovscot.decommissioned.repository;
 import org.mygovscot.decommissioned.model.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
