cbf2f6df0c5d657e9da82f2421692c8846e95dc0
Rupert Smith <rupert.smith2@scotland.gsi.gov.uk>
2015-06-04 12:05:42 +0100

    Added the standard logback configuration to the distribution.

--- decommission-tool-deb/src/deb/run.sh
+++ decommission-tool-deb/src/deb/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-exec /usr/bin/java -jar /opt/decommission-tool/decommission-tool.jar >> /var/log/decommission-tool/decommission-tool.log 2>&1
+exec /usr/bin/java -Dlogging.config=/opt/decommission-tool/logback.xml -jar /opt/decommission-tool/decommission-tool.jar >> /var/log/decommission-tool/decommission-tool.log 2>&1
