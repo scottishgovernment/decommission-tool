7b2385b7701e6bd03a15efbf0372d004b09a50a2
David Sinclair <David.Sinclair@scotland.gsi.gov.uk>
2015-07-02 14:04:08 +0100

    fixed migration script

--- decommission-tool-service/src/main/resources/db/migration/V1_3__UniquenessContraintForSrcUrlNowWithinSite.sql
+++ decommission-tool-service/src/main/resources/db/migration/V1_3__UniquenessContraintForSrcUrlNowWithinSite.sql
@@ -1,2 +1,15 @@
-ALTER TABLE page DROP CONSTRAINT page_src_url_key;
+-- create a copy of the page table but withoput the unioque constraint on src_url
+CREATE TABLE page_copy (
+  id varchar not null primary key,
+  site_id varchar,
+  src_url varchar,
+  target_url varchar,
+  match_level varchar
+);
+insert into page_copy (select * from page);
+
+-- drop the original table and then rename the copy
+drop table page;
+alter table page_copy rename to page;
+
 ALTER TABLE page ADD CONSTRAINT site_and_src_url_unique UNIQUE (site_id, src_url);
