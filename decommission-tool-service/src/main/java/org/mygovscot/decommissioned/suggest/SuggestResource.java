e76c2c691782a962dd38463c494bed201c7c7661
David Sinclair <David.Sinclair@scotland.gsi.gov.uk>
2015-07-20 15:28:53 +0100

    MGVI-2881 improved import (accepts single columns, accepts fragments)

--- decommission-tool-service/src/main/java/org/mygovscot/decommissioned/suggest/SuggestResource.java
+++ decommission-tool-service/src/main/java/org/mygovscot/decommissioned/suggest/SuggestResource.java
@@ -2,9 +2,12 @@ package org.mygovscot.decommissioned.suggest;
 
 import org.mygovscot.decommissioned.model.Job;
 import org.mygovscot.decommissioned.repository.JobRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +21,17 @@ import java.io.IOException;
 @RequestMapping("/redirects/suggest")
 public class SuggestResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SuggestResource.class);
+
     @Autowired
     private SuggestService suggestService;
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+
 
     @RequestMapping(value= "{site}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
@@ -31,6 +40,18 @@ public class SuggestResource {
 
         Job jobObj = jobRepository.findOne(job);
         JobUpdatingSuggesterListener suggesterListener = new JobUpdatingSuggesterListener(jobObj, jobRepository);
-        suggestService.updateSuggestions(site, suggesterListener);
+
+        taskExecutor.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    suggestService.updateSuggestions(site, suggesterListener);
+                } catch (IOException e) {
+                    LOG.error("Error generating suggestions.", e);
+                    jobObj.setStatus("error:" + e.getMessage());
+                    jobRepository.save(jobObj);
+                }
+            }
+        });
     }
 }
