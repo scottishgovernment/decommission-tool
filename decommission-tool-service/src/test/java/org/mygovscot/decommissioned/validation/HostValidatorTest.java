06afdf7ee53f6ca729818df5ad88f9df9ae7e184
David Sinclair <David.Sinclair@scotland.gsi.gov.uk>
2015-08-06 12:46:11 +0100

    MGS-427 added test for HostValidator

---
+++ decommission-tool-service/src/test/java/org/mygovscot/decommissioned/validation/HostValidatorTest.java
@@ -0,0 +1,25 @@
+package org.mygovscot.decommissioned.validation;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HostValidatorTest {
+
+    @Test
+    public void testIsValid() throws Exception {
+        HostValidator sut = new HostValidator();
+        sut.initialize(null);
+
+        assertTrue(sut.isValid("server", null));
+        assertTrue(sut.isValid("server.com", null));
+        assertTrue(sut.isValid("www.server.com", null));
+        assertTrue(sut.isValid("1.2.3.4", null));
+
+        assertFalse(sut.isValid("server server2", null));
+        assertFalse(sut.isValid("1.2.3.4 5.6.7.8 ", null));
+        assertFalse(sut.isValid("http://www.server.com", null));
+        assertFalse(sut.isValid(":www.server.com", null));
+        assertFalse(sut.isValid("www.server.com/", null));
+    }
+}
