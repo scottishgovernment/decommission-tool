27a4ffe034f969f5cf79e1f19121a8e6526fb7ec
David Sinclair <David.Sinclair@scotland.gsi.gov.uk>
2015-09-16 09:58:06 +0100

    MGS-525 increased test coverage for SrcUrlValidator

---
+++ decommission-tool-service/src/test/java/org/mygovscot/decommissioned/validation/SrcUrlValidatorTest.java
@@ -0,0 +1,29 @@
+package org.mygovscot.decommissioned.validation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.mygovscot.decommissioned.model.Page;
+
+public class SrcUrlValidatorTest {
+
+    @Test
+    public void testIsValid() throws Exception {
+        SrcUrlValidator validator = new SrcUrlValidator();
+        assertTrue(validator.isValid(page(Page.MatchType.EXACT, "/"), null));
+        assertTrue(validator.isValid(page(Page.MatchType.EXACT, "/path/with/filename.html"), null));
+        assertTrue(validator.isValid(page(Page.MatchType.PCRE_REGEXP, "[a..z]"), null));
+        assertTrue(validator.isValid(page(Page.MatchType.PCRE_REGEXP, "/*."), null));
+
+        assertFalse(validator.isValid(page(Page.MatchType.PCRE_REGEXP, "["), null));
+        assertFalse(validator.isValid(page(Page.MatchType.PCRE_REGEXP, "["), null));
+    }
+
+    private Page page(Page.MatchType type, String srcUrl) {
+        Page p = new Page();
+        p.setType(type);
+        p.setSrcUrl(srcUrl);
+        return p;
+    }
+}
