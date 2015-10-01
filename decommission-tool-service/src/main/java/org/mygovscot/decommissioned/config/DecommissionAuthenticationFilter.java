e9143864a0f284645e2705a0d3eeecab05f57250
Martin Ellis <martin.ellis@gov.scot>
2015-10-01 12:16:53 +0100

    MGS-572 Replace SecurityContextHolder with that provided by Spring Security.

--- decommission-tool-service/src/main/java/org/mygovscot/decommissioned/config/DecommissionAuthenticationFilter.java
+++ decommission-tool-service/src/main/java/org/mygovscot/decommissioned/config/DecommissionAuthenticationFilter.java
@@ -1,8 +1,10 @@
 package org.mygovscot.decommissioned.config;
 
-import scot.mygov.authentication.client.AuthenticationContext;
-import scot.mygov.authentication.api.UserResource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import scot.mygov.authentication.client.AuthenticationContext;
 import scot.mygov.authentication.spring.SpringAuthenticationFilter;
 
 @Component
@@ -10,16 +12,12 @@ public class DecommissionAuthenticationFilter extends SpringAuthenticationFilter
 
     @Override
     public boolean isAuthorized(AuthenticationContext context) {
-        UserResource user = context.getSession().getUserResource();
-        return user.getRoles().stream()
-                .filter(r -> "admin".equals(r.getName()))
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter("ROLE_ADMIN"::equals)
                 .findFirst()
                 .isPresent();
-    }
-
-    @Override
-    public void onAuthenticatedRequest(AuthenticationContext context) {
-        SecurityContextHolder.setUser(context.getSession().getUserResource().getName());
     }
 
 }
